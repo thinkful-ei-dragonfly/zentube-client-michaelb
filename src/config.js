64b997d51c81ebbe22681d5110084b064b07ae00
michaelbravo777 <michael.bravo777@gmail.com>
2019-07-12 13:57:11 -0400

    changed api endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://zentube.herokuapp.com/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'zentube-client-auth-token',
   VIDEOS_DIR: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/',
   // VIDEOS_DIR: 'http://localhost:8000/media/',
